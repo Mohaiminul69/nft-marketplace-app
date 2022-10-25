c64f30a62238f68aa1b198c0241c35ba93f0386e
mohaiminul69 <mohaiminul69@gmail.com>
2022-10-26 00:07:33 +0600

    first commit

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import Home from "./screens/Home";
 import Details from "./screens/Details";
 import { useFonts } from "expo-font";
